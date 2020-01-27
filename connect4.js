9bce614baf7dfcdf1a9c100ed3d241f8186d138d
bluesh322 <bluesh322@gmail.com>
2020-01-27 09:36:41 -0600

    init

---
+++ connect4.js
@@ -0,0 +1,156 @@
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
+
+const WIDTH = 7;
+const HEIGHT = 6;
+
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+/** makeBoard: create in-JS board structure:
+ *    board = array of rows, each row is array of cells  (board[y][x])
+ */
+
+const makeBoard = () => {
+  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for(let i = 0; i < HEIGHT; i++) {
+      board.push([]);
+      for(let j = 0; j < WIDTH; j++) {
+        board[i][j] = null;            
+      }
+  }    
+}
+
+/** makeHtmlBoard: make HTML table and row of column tops. */
+
+const makeHtmlBoard = () => {
+  let htmlBoard = document.querySelector('#board')
+
+  // Create first row where tiles can be added with 'click'
+  let top = document.createElement("tr");
+  top.setAttribute("id", "column-top");
+  top.addEventListener("click", handleClick);
+
+  for (var x = 0; x < WIDTH; x++) {
+    let headCell = document.createElement("td");
+    headCell.setAttribute("id", x);
+    top.append(headCell);
+  }
+  htmlBoard.append(top);
+
+  // Create rest of the blank tiles where the game will be played
+  for (let y = 0; y < HEIGHT; y++) {
+    const row = document.createElement("tr");
+    for (let x = 0; x < WIDTH; x++) {
+      const cell = document.createElement("td");
+      cell.setAttribute("id", `${y}-${x}`); //gives each tile an id
+      row.append(cell);
+    }
+    htmlBoard.append(row);
+  }
+}
+
+/** findSpotForCol: given column x, return top empty y (null if filled) */
+
+const findSpotForCol = (x) => {
+  // TODO: write the real version of this, rather than always returning 0
+  for(let y = HEIGHT-1; y >= 0; y--) {
+    if(!board[y][x]) {
+      return y;
+    }
+  }
+  return null;
+}
+
+/** placeInTable: update DOM to place piece into HTML table of board */
+
+const placeInTable = (y, x) => {
+  // TODO: make a div and insert into correct table cell
+  let cell = document.getElementById(`${y}-${x}`);
+  let tile = document.createElement('div');
+  tile.classList.add('piece');
+  tile.classList.add(`p${currPlayer}`);
+  cell.append(tile);
+}
+
+/** endGame: announce game end */
+
+const endGame = (msg) => {
+  // TODO: pop up alert message
+  alert("You've managed to tie");
+}
+
+/** handleClick: handle click of column top to play piece */
+
+const handleClick = (evt) => {
+  // get x from ID of clicked cell
+  let x = +evt.target.id;
+  // get next spot in column (if none, ignore click)
+  let y = findSpotForCol(x);
+  if (y === null) {
+    return;
+  }
+
+  // place piece in board and add to HTML table
+  // TODO: add line to update in-memory board
+  board[y][x] = currPlayer;
+  placeInTable(y, x);
+
+  // check for win
+  if (checkForWin()) {
+    return endGame(`Player ${currPlayer} won!`);
+  }
+
+  // check for tie
+  // TODO: check if all cells in board are filled; if so call, call endGame
+  if(board.every((row) => row.every(tile => tile))) {
+    return endGame(`You managed to tie somehow`);
+  }
+  // switch players
+  // TODO: switch currPlayer 1 <-> 2
+  currPlayer === 1 ? currPlayer = 2 : currPlayer = 1;
+}
+
+/** checkForWin: check board cell-by-cell for "does a win start here?" */
+
+const checkForWin = () => {
+  const _win = (cells) => {
+    // Check four cells to see if they're all color of current player
+    //  - cells: list of four (y, x) cells
+    //  - returns true if all are legal coordinates & all match currPlayer
+
+    return cells.every(
+      ([y, x]) =>
+        y >= 0 &&
+        y < HEIGHT &&
+        x >= 0 &&
+        x < WIDTH &&
+        board[y][x] === currPlayer
+    );
+  }
+
+  // loop through the entirety of the board each turn
+  // check for each possible win condition
+  //horiz and vert are easiest with -- or | lines
+  //diag Down Right makes a diagonal down right with x y
+  //diag Down Left does the opposite.
+  for (var y = 0; y < HEIGHT; y++) {
+    for (var x = 0; x < WIDTH; x++) {
+      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+
+      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+        return true;
+      }
+    }
+  }
+}
+
+makeBoard();
+makeHtmlBoard();
